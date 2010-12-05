405e45568ce5d7c02c44a86ac260e1b2a036ed13
rybolov <rybolov@ryzhe.ath.cx>
2010-12-05 00:06:11 -0500

    Working on the post body

--- index.php
+++ index.php
@@ -8,32 +8,36 @@ if (!@include 'config.php') {
     die('Config not found.  Please read README.');
 }
 
-$postbody = http_get_request_body();
-echo ("$postbody");
+//This supposedly works with the PECL HTTP 
+//$postbody = http_get_request_body();
+
+//This works on most PHP installs
+$postbody = @file_get_contents('php://input');
+//echo ("$postbody");
 
 
+$looper = 0;
 
-
-
-//echo ("$logline");
-
-//"syslog" mode
-if ($mode == "syslog")
+while ($postbody as $logline)
 {
-    //syslog($syslogpriority | $syslogfacility, $syslogident.': '. $logmessage);
+    echo $looper;
+    echo ("$logline\r");
     
+    //"syslog" mode
+    if ($mode == "syslog")
+    {
+        //syslog($syslogpriority | $syslogfacility, $syslogident.': '. $logmessage);
+        
+    }
+    
+    //"direct" mode
+    elseif ($mode == "direct")
+    {
+        //check for "wait"
+        //write to file
+    }
+
 }
-
-
-
-
-//"direct" mode
-if ($mode == "direct")
-{
-    //check for "wait"
-    //write to file
-}
-
 
 
 ?> 
